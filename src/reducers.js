a35891c5d4f98c37e87fe41eaa9b17595befdbfa
SanjuManna <sanjumanna2201@gmail.com>
2023-03-19 13:47:36 +0530

    Commit

--- src/reducers.js
+++ src/reducers.js
@@ -4,12 +4,15 @@ const reducer = (state, action) => {
     switch (action.type) {
         case "add":
             action.setBalance(action.balance + 10);
-            return [...state, action.name];
+            return [...state, { name: action.name, id: uuidv4() }];
 
         case "fuel":
             if (action.balance > 50) {
                 action.setBalance(action.balance - 50);
             }
+            return state;
+
+        default:
             return state;
     }
 };
